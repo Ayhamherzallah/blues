db3a9d86b52eafe1419936cbaa74bec895eac148
ayham <ayhamherzallah@gmail.com>
2023-09-05 02:46:05 +0300

    deploy

--- home/static/js/home.js
+++ home/static/js/home.js
@@ -9,7 +9,6 @@ const observerOptions = {
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         entry.target.classList.toggle('show', entry.isIntersecting)
-        if(entry.isIntersecting) observer.unobserve(entry.target)
     })
 },observerOptions)
 
